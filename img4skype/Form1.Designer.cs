0a0bdb0db9dac9c3446f35820d06b9833baa80f8
aruz <alxruz@gmail.com>
2012-01-19 07:17:57 +0600

    Signed-off-by: aruz <alxruz@gmail.com>

---
+++ img4skype/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace img4skype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(223, 97);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "50";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(223, 168);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 193);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Source image";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 240);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(235, 135);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Location = new System.Drawing.Point(253, 41);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(302, 92);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Convert options";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(50, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Error";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Height";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.pictureBox2);
+            this.groupBox4.Location = new System.Drawing.Point(253, 139);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(302, 236);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Preview";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(290, 211);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 387);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ img4skype/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace img4skype
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _src;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var result = new Bitmap(width, height);
+
+            using (var graphics = Graphics.FromImage(result))
+            {
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                graphics.DrawImage(image, 0, 0, result.Width, result.Height);
+            }
+
+            return result;
+        }
+
+        public bool ColorCmp(Color color1, Color color2, int e)
+        {
+            var d = Math.Abs(color1.R - color2.R) + Math.Abs(color1.G - color2.G) + Math.Abs(color1.B - color2.B);
+            return d/3 <= e;
+        }
+
+        public string PrintColor(Color color)
+        {
+            return color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+        }
+
+        class ColorSum
+        {
+            public Color Color;
+            public int Sum;
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            _src = new Bitmap(openFileDialog1.FileName);
+
+            pictureBox1.Image = _src;
+        }
+
+        private void Button2Click(object sender, EventArgs e)
+        {
+            var rate = _src.Height/(double) _src.Width;
+            var h = Convert.ToInt32(textBox1.Text);
+            var img = ResizeImage(_src, Convert.ToInt32(h * 1.5), Convert.ToInt32(h * rate));
+
+            h *= 4;
+            var preview = new Bitmap(Convert.ToInt32(h*1.5), Convert.ToInt32(h * rate));
+
+            var d = Convert.ToInt32(textBox2.Text);
+
+            var list = new List<ColorSum>();
+            for (var i = 0; i < img.Height; i++)
+                for (var j = 0; j < img.Width; j++)
+                {
+                    var color = img.GetPixel(j, i);
+                    if (ColorCmp(color, Color.White, d)) continue;
+                    var colorSet = list.FirstOrDefault(c => ColorCmp(c.Color, color, d));
+                    if (colorSet == null)
+                        list.Add(new ColorSum {Color = color, Sum = 1});
+                    else
+                        colorSet.Sum++;
+                }
+
+            var maxColor = list.First(c => c.Sum == list.Max(c1 => c1.Sum)).Color;
+
+            var data = "<font color=\"#" + PrintColor(maxColor) + "\" size=\"1\"><b>";
+
+            var pcolor = new Color();
+            var fcolor = new Color();
+            var fonttag = false;
+            var utag = false;
+
+            for (var i = 0; i < img.Height; i++)
+            {
+                for (var j = 0; j < img.Width; j++)
+                {
+                    var color = img.GetPixel(j, i);
+
+                    if (ColorCmp(color, maxColor, d))
+                    {
+                        if(fonttag)
+                        {
+                            data += "</font>";
+                            fonttag = false;
+                        }
+                        if (!utag)
+                        {
+                            data += "<u>";
+                            utag = true;
+                        }
+                        data += "█";
+                        PreviewSetPixel(preview, j, i, maxColor);
+                        continue;
+                    }
+
+                    if (ColorCmp(color, Color.White, d))
+                    {
+                        if (fonttag)
+                        {
+                            data += "</font>";
+                            fonttag = false;
+                        }
+                        if (utag)
+                        {
+                            data += "</u>";
+                            utag = false;
+                        }
+                        data += "  ";
+                        PreviewSetPixel(preview, j, i, Color.White);
+                        continue;
+                    }
+
+                    if (!ColorCmp(color, pcolor, d))
+                    {
+                        if (fonttag)
+                        {
+                            data += "</font>";
+                            fonttag = false;
+                        }
+                    }
+
+                    if (!fonttag)
+                    {
+                        if (!utag)
+                        {
+                            data += "<u>";
+                            utag = true;
+                        }
+                        data += "<font color=\"#" + PrintColor(color) + "\">█";
+                        fonttag = true;
+                        fcolor = color;
+                        PreviewSetPixel(preview, j, i, fcolor);
+                    }
+                    else
+                    {
+                        data += "█";
+                        PreviewSetPixel(preview, j, i, fcolor);
+                    }
+                    pcolor = color;
+
+                }
+                data += Environment.NewLine;
+            }
+
+            if (fonttag) data += "</font>";
+            if (utag) data += "</u>";
+            data += "</b></font>";
+
+            pictureBox2.Image = preview;
+            label1.Text = data.Length.ToString();
+
+            richTextBox1.Text = data;
+        }
+
+        private void PreviewSetPixel(Bitmap p, int x, int y, Color c)
+        {
+            for (var i = 0; i < 2; i++)
+                for (var j = 0; j < 3; j++)
+                    p.SetPixel(x * 2 + i, y * 3 + j, Color.FromArgb(c.R, c.G, c.B));
+
+        }
+    }
+}
