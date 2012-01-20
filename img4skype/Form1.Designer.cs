d2662f610f75343fb54a5f05d22425e0935bba3a
aruz <alxruz@gmail.com>
2012-01-20 17:03:55 +0600

    new algorithm of DenisNikolayev

--- img4skype/Form1.Designer.cs
+++ img4skype/Form1.Designer.cs
@@ -29,23 +29,20 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -61,19 +58,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1Click);
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(6, 19);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(223, 97);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(50, 19);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(55, 20);
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "50";
             // 
@@ -93,7 +82,7 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(253, 12);
+            this.button2.Location = new System.Drawing.Point(12, 39);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -101,35 +90,15 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 119);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(13, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "0";
-            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.pictureBox1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Location = new System.Drawing.Point(12, 68);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(235, 193);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Source image";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.richTextBox1);
-            this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(12, 240);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(235, 135);
-            this.groupBox2.TabIndex = 7;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Result";
             // 
             // groupBox3
             // 
@@ -137,25 +106,25 @@
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.label2);
             this.groupBox3.Controls.Add(this.textBox1);
-            this.groupBox3.Location = new System.Drawing.Point(253, 41);
+            this.groupBox3.Location = new System.Drawing.Point(93, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(302, 92);
+            this.groupBox3.Size = new System.Drawing.Size(154, 50);
             this.groupBox3.TabIndex = 8;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Convert options";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(50, 45);
+            this.textBox2.Location = new System.Drawing.Point(119, 19);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(55, 20);
+            this.textBox2.Size = new System.Drawing.Size(28, 20);
             this.textBox2.TabIndex = 5;
             this.textBox2.Text = "10";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Location = new System.Drawing.Point(84, 22);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 13);
             this.label3.TabIndex = 4;
@@ -173,9 +142,9 @@
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.pictureBox2);
-            this.groupBox4.Location = new System.Drawing.Point(253, 139);
+            this.groupBox4.Location = new System.Drawing.Point(253, 68);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(302, 236);
+            this.groupBox4.Size = new System.Drawing.Size(229, 193);
             this.groupBox4.TabIndex = 9;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Preview";
@@ -184,52 +153,58 @@
             // 
             this.pictureBox2.Location = new System.Drawing.Point(6, 19);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(290, 211);
+            this.pictureBox2.Size = new System.Drawing.Size(217, 168);
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "HTML-code will be copied to the clipboard";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 387);
+            this.ClientSize = new System.Drawing.Size(497, 272);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- img4skype/Form1.cs
+++ img4skype/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using System.Text;
 
 namespace img4skype
 {
@@ -31,22 +32,6 @@ namespace img4skype
             return result;
         }
 
-        public bool ColorCmp(Color color1, Color color2, int e)
-        {
-            var d = Math.Abs(color1.R - color2.R) + Math.Abs(color1.G - color2.G) + Math.Abs(color1.B - color2.B);
-            return d/3 <= e;
-        }
-
-        public string PrintColor(Color color)
-        {
-            return color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
-        }
-
-        class ColorSum
-        {
-            public Color Color;
-            public int Sum;
-        }
 
         private void Button1Click(object sender, EventArgs e)
         {
@@ -56,132 +41,250 @@ namespace img4skype
             pictureBox1.Image = _src;
 
             button2.Enabled = true;
+            textBox2.Text = "0";
         }
+
+
+        private IEnumerable<Color?> Scan(Bitmap bitmap)
+        {
+            for (var y = 0; y < bitmap.Height; y++)
+            {
+                for (var x = 0; x < bitmap.Width; x++)
+                {
+                    yield return bitmap.GetPixel(x, y);
+                }
+                yield return null;
+            }
+        }
+
+
+        private Dictionary<Color, Color> CorrectColors(IEnumerable<Color?> scaned, Bitmap img, int error)
+        {
+            Func<int, int> D = v => v * v;
+
+            Func<Color, Color, bool> colorCmp = (color1, color2) =>
+                error < 100 && (color1 == Color.White || color2 == Color.White) ? false :
+                Math.Sqrt(D(color1.R - color2.R) + D(color1.G - color2.G) + D(color1.B - color2.B)) <= error;
+
+            var mapColors = scaned
+                .Where(c => c != null)
+                .Select(row => row.Value)
+                .GroupBy(row => row)
+                .ToDictionary(row => row.Key, row => new { newColor = row.Key, count = row.Count() });
+
+            var processed = new HashSet<Color>();
+
+            foreach (var color in mapColors.OrderByDescending(row => row.Value.count).Select(row => row.Key).ToArray())
+            {
+                if (processed.Add(color))
+                {
+                    foreach (var oldcolor in mapColors.Keys.ToArray().Where(c => !processed.Contains(c)).Where(c => colorCmp(color, c)))
+                    {
+                        processed.Add(oldcolor);
+                        mapColors[oldcolor] = new { newColor = color, count = 0 };
+                    }
+                }
+            }
+
+            return mapColors.ToDictionary(row => row.Key, row => row.Value.newColor);
+        }
+
+
+        private Tuple<Action<Color?, int>, Action> Preview(int width, int height, Action<Bitmap> onCustomFinish)
+        {
+            var preview = new Bitmap(width * 4, height * 4);
+
+            Action<int, int, Color> previewSetPixel = (x, y, c) =>
+            {
+                for (var i = 0; i < 2; i++)
+                    for (var j = 0; j < 3; j++)
+                        preview.SetPixel(x * 2 + i, y * 3 + j, c);
+            };
+
+            int lastx = 0, lasty = 0;
+            Action<Color?, int> write = (c, len) =>
+            {
+                if (c != null)
+                {
+                    for (var x = 0; x < len; x++)
+                    {
+                        previewSetPixel(lastx + x, lasty, c.Value);
+                    }
+                    lastx += len;
+                }
+                else
+                {
+                    lastx = 0;
+                    lasty++;
+                }
+            };
+
+            Action onFinish = () => onCustomFinish(preview);
+
+            return new Tuple<Action<Color?, int>, Action>(write, onFinish);
+        }
+
+        private Tuple<Action<Color?, int>, Action> ToSkype(Color backgroundColor, Action<string> onCustomFinish)
+        {
+            Func<Color, string> printColor = (color) => "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+
+            var result = new StringBuilder();
+            result.AppendFormat("<font color='{0}' size='1'><b><u>", printColor(backgroundColor));
+
+            Action<Color?, int> write = (color, len) =>
+            {
+                if (color != null)
+                {
+                    if (color != backgroundColor)
+                    {
+                        result.AppendFormat("<font color='{0}'>{1}</font>", printColor(color.Value),
+                                                new string('█', len));
+                    }
+                    else
+                    {
+                        result.Append(new string('█', len));
+                    }
+                }
+                else
+                {
+                    result.AppendLine();
+                }
+            };
+
+            Action onFinish = () =>
+            {
+                result.Append("</u></b></font>");
+                onCustomFinish(result.ToString());
+            };
+
+            return new Tuple<Action<Color?, int>, Action>(write, onFinish);
+        }
+
 
         private void Button2Click(object sender, EventArgs e)
         {
-            var rate = _src.Height/(double) _src.Width;
+            var rate = _src.Height / (double)_src.Width;
             var h = Convert.ToInt32(textBox1.Text);
             var img = ResizeImage(_src, Convert.ToInt32(h * 1.5), Convert.ToInt32(h * rate));
+            var scaned = Scan(img).ToArray();
 
-            h *= 4;
-            var preview = new Bitmap(Convert.ToInt32(h*1.5), Convert.ToInt32(h * rate));
+            var error = Convert.ToInt32(textBox2.Text);
 
-            var d = Convert.ToInt32(textBox2.Text);
-
-            var list = new List<ColorSum>();
-            for (var i = 0; i < img.Height; i++)
-                for (var j = 0; j < img.Width; j++)
-                {
-                    var color = img.GetPixel(j, i);
-                    color = color.A == 0 ? Color.White : Color.FromArgb(color.R, color.G, color.B);
-
-                    if (ColorCmp(color, Color.White, d)) continue;
-                    var colorSet = list.FirstOrDefault(c => ColorCmp(c.Color, color, d));
-                    if (colorSet == null)
-                        list.Add(new ColorSum {Color = color, Sum = 1});
-                    else
-                        colorSet.Sum++;
-                }
-
-            var maxColor = list.First(c => c.Sum == list.Max(c1 => c1.Sum)).Color;
-
-            var data = "<font color=\"#" + PrintColor(maxColor) + "\" size=\"1\"><b>";
-
-            var pcolor = new Color();
-            var fcolor = new Color();
-            var fonttag = false;
-            var utag = false;
-
-            for (var i = 0; i < img.Height; i++)
+            // попробуем подобрать минимальное возможное колличество ошибок
+            if (error == 0)
             {
-                for (var j = 0; j < img.Width; j++)
+                error = Enumerable.Range(0, int.MaxValue).BinarySearch(err =>
                 {
-                    var color = img.GetPixel(j, i);
-                    color = color.A == 0 ? Color.White : Color.FromArgb(color.R, color.G, color.B);
+                    var mc = CorrectColors(scaned, img, err);
 
-                    if (ColorCmp(color, maxColor, d))
-                    {
-                        if(fonttag)
+                    var size = 0;
+                    var checkSize = ToSkype(
+                        mc.GroupBy(row => row.Value).OrderByDescending(row => row.Count()).First().Key,
+                        (result) =>
                         {
-                            data += "</font>";
-                            fonttag = false;
-                        }
-                        if (!utag)
-                        {
-                            data += "<u>";
-                            utag = true;
-                        }
-                        data += "█";
-                        PreviewSetPixel(preview, j, i, maxColor);
-                        continue;
-                    }
+                            size = result.Length + result.Count(c => c == '█') * 2;
+                        });
 
-                    if (ColorCmp(color, Color.White, d))
-                    {
-                        if (fonttag)
-                        {
-                            data += "</font>";
-                            fonttag = false;
-                        }
-                        if (utag)
-                        {
-                            data += "</u>";
-                            utag = false;
-                        }
-                        data += "  ";
-                        PreviewSetPixel(preview, j, i, Color.White);
-                        continue;
-                    }
+                    MergeColumns(
+                        scaned,
+                        mc,
+                        write: checkSize.Item1,
+                        finish: checkSize.Item2
+                     );
 
-                    if (!ColorCmp(color, pcolor, d))
-                    {
-                        if (fonttag)
-                        {
-                            data += "</font>";
-                            fonttag = false;
-                        }
-                    }
+                    return size < 30000;
+                });
 
-                    if (!fonttag)
-                    {
-                        if (!utag)
-                        {
-                            data += "<u>";
-                            utag = true;
-                        }
-                        data += "<font color=\"#" + PrintColor(color) + "\">█";
-                        fonttag = true;
-                        fcolor = color;
-                        PreviewSetPixel(preview, j, i, fcolor);
-                    }
-                    else
-                    {
-                        data += "█";
-                        PreviewSetPixel(preview, j, i, fcolor);
-                    }
-                    pcolor = color;
-
-                }
-                data += Environment.NewLine;
+                textBox2.Text = error.ToString();
             }
 
-            if (fonttag) data += "</font>";
-            if (utag) data += "</u>";
-            data += "</b></font>";
+            // вывод
+            Dictionary<Color, Color> mapColors = CorrectColors(scaned, img, error);
+            var preview = Preview(img.Width, img.Height, result => pictureBox2.Image = result);
+            var toSkype = ToSkype(
+                mapColors.GroupBy(row => row.Value).OrderByDescending(row => row.Count()).First().Key,
+                (result) =>
+                {
+                    Clipboard.SetText(result);
+                    label1.Text = "Symbols: " + (result.Length + result.Count(c => c == '█') * 2).ToString() + " (copied to the clipboard)";
+                });
 
-            pictureBox2.Image = preview;
-            label1.Text = data.Length.ToString();
-
-            richTextBox1.Text = data;
+            MergeColumns(scaned, mapColors,
+                write: (Action<Color?, int>)Delegate.Combine(preview.Item1, toSkype.Item1),
+                finish: (Action)Delegate.Combine(preview.Item2, toSkype.Item2)
+             );
         }
 
-        private void PreviewSetPixel(Bitmap p, int x, int y, Color c)
+        private void MergeColumns(IEnumerable<Color?> scaned, Dictionary<Color, Color> mapColors, Action<Color?, int> write, Action finish)
         {
-            for (var i = 0; i < 2; i++)
-                for (var j = 0; j < 3; j++)
-                    p.SetPixel(x * 2 + i, y * 3 + j, c);
+            var bufferLen = 0;
+            Color? bufferColor = null;
+            foreach (var c in scaned)
+            {
+                if (c != null)
+                {
+                    var nc = mapColors[c.Value];
+                    if (bufferColor != null)
+                    {
+                        if (bufferColor == nc)
+                        {
+                            bufferLen += 1;
+                        }
+                        else
+                        {
+                            write(bufferColor, bufferLen);
+                            bufferColor = nc;
+                            bufferLen = 1;
+                        }
+                    }
+                    else
+                    {
+                        bufferColor = nc;
+                        bufferLen = 1;
+                    }
+                }
+                else
+                {
+                    if (bufferColor != null)
+                    {
+                        write(bufferColor, bufferLen);
+                        bufferColor = null;
+                        bufferLen = 0;
+                    }
 
+                    write(null, 0);
+                }
+            }
+
+            finish();
+        }
+
+
+    }
+
+    public static class Ext
+    {
+        public static T BinarySearch<T>(this IEnumerable<T> ar, Func<T, bool> check)
+        {
+            var en = ar.GetEnumerator();
+            var buffer = new List<T>();
+
+            var found = false;
+            do
+            {
+                buffer.Clear();
+                for (var i = 0; i < 5; i++)
+                {
+                    en.MoveNext();
+                    buffer.Add(en.Current);
+                }
+
+                found = check(buffer.Last());
+            }
+            while (!found);
+
+            buffer.Reverse();
+            return buffer.TakeWhile(check).Last();
         }
     }
 }
